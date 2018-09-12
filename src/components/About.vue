de9810090d305f02f7a2362bab2393243af6958e
Rib1356 <rib1356@hotmail.co.uk>
2018-09-12 11:27:59 +0100

    Test commit to push new branch to heroku

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-        <p>This page will be used for something</p>
+        <p>This page will be used for the admin preferences!</p>
         <p>Currently just populating the page navigation</p>
     </div>
 </template>
+
+<script>
+export default {
+    //Test
+}
+</script>
+
